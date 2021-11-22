f32ff7ae6893b00a832a697e4eb3cdefdb659477
Chris Maxwell <chris.maxwell@gmail.com>
2021-11-22 07:33:42 -0800

    comment for using codebuild

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Usage
 
+Please deploy updates through the atlantis-base codebuild project in the wink-tools aws account.
+
 ```bash
 ./build.sh
-./push.sh
 ```
--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo "Please deploy updates through the atlantis-base codebuild project in the wink-tools aws account."
+source ./variables.sh
 
-# source ./variables.sh
+tag=${CONTAINER}/${BUILD_NAME}:${BUILD_VERSION}
 
-# tag=${CONTAINER}/${BUILD_NAME}:${BUILD_VERSION}
+docker build \
+  --build-arg GITHUB_DEPLOY_KEY="$GITHUB_DEPLOY_KEY" \
+  -t ${tag} \
+  .
 
-# docker build \
-#   --build-arg GITHUB_DEPLOY_KEY="$GITHUB_DEPLOY_KEY" \
-#   -t ${tag} \
-#   .
-
-# echo "setting ${tag} to latest"
-# docker tag ${tag} ${CONTAINER}/${BUILD_NAME}:latest
+echo "setting ${tag} to latest"
+docker tag ${tag} ${CONTAINER}/${BUILD_NAME}:latest
